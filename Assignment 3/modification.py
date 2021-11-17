0919952f3e07b880f2cd6dec3decb7ac7ec46dc2
Ajay <ajaycc17@gmail.com>
2021-11-17 23:07:42 +0530

    add project2

---
+++ Assignment 3/modification.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# read csv file
+df = pd.read_csv('weather_data_2sites.csv')
+
+# convert to datetime format
+df['timestamp'] = pd.to_datetime(df.timestamp)
+
+# get day first
+df['timestamp'] = df['timestamp'].dt.strftime('%d/%m/%Y %H:%M:%S')
+
+# rename id column
+df.rename( columns={'Unnamed: 0':'id'}, inplace=True )
+
+# save the modified CSV data
+df.to_csv('weather_data_2sites.csv', index=False)
